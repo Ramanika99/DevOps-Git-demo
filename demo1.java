336bcdbf2d6c7e690e0a947585b55532748f7275
Ramanika <ramanikadighade414@gmail.com>
2023-05-14 08:58:58 +0000

    DevOps chages Added

---
+++ demo1.java
@@ -0,0 +1,9 @@
+public class Demo{
+	public static void main(String args[]){
+		//Print statement for hello world
+		System.out.println("Hello World");
+		//Print statement
+		System.out.println("Welcom to DevOps");
+	
+	}
+}
